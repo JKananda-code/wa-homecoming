bc3bc9c775c2bc78819f15e1d9979756d4d0e9ef
John Kananda <jkananda5@outlook.com>
2025-07-08 15:27:15 -0600

    commit 08-07am after adding some Images

--- app/components/ImageGallery.tsx
+++ app/components/ImageGallery.tsx
@@ -1,4 +1,4 @@
-
+import Image from "next/image";
 export default function ImageGallery({ images }: { images: { src: string; caption: string }[] }) {
   return (
     <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
