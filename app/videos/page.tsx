8d6ee6fddee8a8fc0a9c5a730cb8dd893ce37b41
John Kananda <jkananda5@outlook.com>
2025-07-14 11:42:51 -0600

    commit 14-12 noon after adding header Image ti Vg

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -1,14 +1,33 @@
 import React from 'react'
+import Image from 'next/image'
+
 
 const VideoPlayer = () => {
+    
     return (
-        <div className="video-player">
+   <>
+        <div className="relative h-screen  mt-2">
+                 <Image
+                  src="/Mulima-Tosha.jpg"
+                  alt="Mount Kenya Photo"
+                  width={1200}
+                  height={800}
+                  priority
+                  className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                  />
+         </div>
+         <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-7xl text-black bg-yellow-300 font-bold">
+         Mulima-Tosha
+         </h1>
+        <div>       
         <video width="640" height="360" controls className="mx-auto mb-4">
             <source src="/VID-20250630-WA0011.mp4" type="video/mp4" />
             Your browser does not support the video tag.
         </video>
         <p>VG Group Members Coming Home</p>
         </div>
+  </>   
     )
+   
 }
 export default VideoPlayer;
