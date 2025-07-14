bc777deda48c1f2b477cef2fefe05eae77e5cfa5
John Kananda <jkananda5@outlook.com>
2025-07-14 00:09:05 -0600

    commit 14-12.15am  after adding some Images

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const VideoPlayer = () => {
     return (
-        <div className=" justify justify-items center video-player">
-        <video controls>
+        <div className="video-player">
+        <video width="640" height="360" controls className="mx-auto mb-4">
             <source src="/VID-20250630-WA0011.mp4" type="video/mp4" />
             Your browser does not support the video tag.
         </video>
--- app/watch/[id]/page.js
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import MuxPlayer from '/mux/mux-player-react';
-
-export default async function Page({ params }) {
-  const asset = getAssetFromDatabase(params.id);
-  return <MuxPlayer streamType="on-demand" playbackId={asset.id} accentColor="#ac39f2" />;
-}
