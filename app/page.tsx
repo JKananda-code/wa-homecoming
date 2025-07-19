4757b40b2fc918b4bab8e8b961e35e3415fc1fad
John Kananda <jkananda5@outlook.com>
2025-07-18 21:51:04 -0600

    commit 18-07 6.00pm after adding Phone menu

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         width={800}
         height={600}
       />
-      <h1 className="text-3xl font-bold text-center text-emerald-700 mb-4">Welcome to Wanjiku Homecoming</h1>
+      <h1 className="text-3xl font-bold text-center text-emerald-700 mb-4 mt-5 sm:mt-2">Wanjiku & Son Homecoming Celebrations</h1>
       <p className="text-center text-gray-700">
         Celebrating the rich cultural heritage of the Kikuyu community through the Kamweretho ceremony.
       </p>
